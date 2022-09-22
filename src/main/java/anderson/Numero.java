0531692af22254e2a62a9ec4d3c3eb3c837530ec
José Anderson <jose.andersonjr2@gmail.com>
2022-09-22 16:22:10 -0300

    Add exercicio Numero

--- src/main/java/anderson/Numero.java
+++ src/main/java/anderson/Numero.java
@@ -1,4 +1,17 @@
 package anderson;
 
+import javax.swing.*;
+
 public class Numero {
+    public static void main(String[] args){
+        var numero = Integer.parseInt(JOptionPane.showInputDialog("Insira um número: "));
+        var resto = numero%2;
+        if (resto == 0){
+            JOptionPane.showMessageDialog(null, "O número inserido é par! ");
+        }else{
+            JOptionPane.showMessageDialog(null, "O número inserido é ímpar!");
+        }
+
+    }
+
 }
